39dd0cff8840b64cfcb81596355fbbcd276d3c6c
frn-teq <francisco.tequida00@gmail.com>
2022-07-13 15:14:09 -0700

    feat: track every time a product is requested

--- api/products/views.py
+++ api/products/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-from products.models import Product
+from rest_framework.response import Response
+from products.models import Product, ProductTrack
 from products.serializers import ProductSerializer
 from permissions.product_permissions import ProductPermission
 
@@ -10,3 +11,18 @@ class ProductModelViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [ProductPermission]
     lookup_field = "slug"
+
+
+    def retrieve(self, request, *args, **kwargs):
+        """Override retrieve method to update the amount of times a single
+        item is requested.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        if request.user is None:
+            ProductTrack.objects.create(product=instance)
+        elif request.user.is_staff is False:
+            ProductTrack.objects.create(product=instance, user=request.user)
+
+        return Response(serializer.data)
